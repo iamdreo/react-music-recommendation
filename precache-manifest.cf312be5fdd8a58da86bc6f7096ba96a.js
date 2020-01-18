26377a78f4bf1f7689d3e17d23116f31260435d6
DRE <andrew_ole@yahoo.com>
2020-01-18 15:59:26 +0100

    Updates

---
+++ precache-manifest.cf312be5fdd8a58da86bc6f7096ba96a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1d5452afa5398318096444bf076937",
+    "url": "/react-music-recommendation/index.html"
+  },
+  {
+    "revision": "c0eb182fdadd4b26a3eb",
+    "url": "/react-music-recommendation/static/css/2.e0a7667c.chunk.css"
+  },
+  {
+    "revision": "96b70b2199f44ded2a9d",
+    "url": "/react-music-recommendation/static/css/main.a1fe19f2.chunk.css"
+  },
+  {
+    "revision": "c0eb182fdadd4b26a3eb",
+    "url": "/react-music-recommendation/static/js/2.5b05f9df.chunk.js"
+  },
+  {
+    "revision": "8eec97e09a57203e60cbb3c5c24b2428",
+    "url": "/react-music-recommendation/static/js/2.5b05f9df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96b70b2199f44ded2a9d",
+    "url": "/react-music-recommendation/static/js/main.615900ad.chunk.js"
+  },
+  {
+    "revision": "bd2851386783e9f37baf",
+    "url": "/react-music-recommendation/static/js/runtime-main.c9482276.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-music-recommendation/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-music-recommendation/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-music-recommendation/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-music-recommendation/static/media/slick.f97e3bbf.svg"
+  }
+]);
